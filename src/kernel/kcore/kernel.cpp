834686844d0f2b2cf159666a12cbf6a52ba4841e
MalTheLegend104 <79492634+MalTheLegend104@users.noreply.github.com>
2022-12-28 02:01:16 -0500

    buffer test

--- src/kernel/kcore/kernel.cpp
+++ src/kernel/kcore/kernel.cpp
@@ -8,5 +8,5 @@ extern "C" {
 void kernel_main(void){
     clearVGABuf();
     set_color_vga(VGA_COLOR_YELLOW, VGA_COLOR_BLACK);
-    puts_vga("This is a really long message to test the way the buffer works.");
+    puts_vga("This is a really long message to test the way the buffer works. qwertyuiop[]\\asdfghjkl;'zxcvbnm,./1234567890-=!@#$%^&*()_+\"?<>{}|");
 }
