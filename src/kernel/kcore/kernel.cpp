3f9223df72d6dceabebb23701e1853bda7d4a6ca
MalTheLegend104 <79492634+MalTheLegend104@users.noreply.github.com>
2023-01-27 01:06:12 -0500

    added docs

--- src/kernel/kcore/kernel.cpp
+++ src/kernel/kcore/kernel.cpp
@@ -1,9 +1,24 @@
-#include <klibc/kprint.h>
-#include <panic.h>
 #include <stdlib.h>
-#include <cpuid.h>
 #include <string.h>
+#include <cpuid.h>
+#include <panic.h>
+#include <klibc/kprint.h>
 #include <klibc/cpuid_calls.h>
+#include <klibc/logger.h>
+
+cpu_features features;
+
+/*
+ * Okay, this is where the fun begins. Literally and figuratively.
+ * We mark these extern c because we need to call it from asm,
+ * because asm can't see c++ functions. Cool, no big deal.
+ * But you may be saying, "if we mark it extern c we can't use c++ features."
+ * You sweet sweet child. Welcome to OSDEV. We torture compilers and break languages.
+ * Everything here is C++.
+ * You can use templates. You can use classes. You can use namespaces.
+ * We are just TRICKING the linker into thinking this is C.
+ * The linker trust us. It shouldn't. This isn't the only time we abuse it.
+ */
 extern "C" {
 	void kernel_main(void);
 	void __cxa_pure_virtual() { }; // needed for pure virtual functions
@@ -11,23 +26,17 @@ extern "C" {
 
 void kernel_main(void) {
 	clearVGABuf();
+	set_colors(VGA_COLOR_YELLOW, VGA_COLOR_BLACK);
 	// Do stuff that needs to be enabled before interrupts here.
 
 	// Enable interrupts
 
 	// Everything else
-	set_colors(VGA_COLOR_YELLOW, VGA_COLOR_BLACK);
 
 	// Print the CPU vendor ID
 	// puts_vga("Vendor ID: ");
 	// puts_vga(vendorID());
 	// putc_vga('\n');
-	char buf[10];
-	for (int i = 0; i < 100; i++) {
-		puts_vga("Loop: ");
-		puts_vga(itoa(i, buf, 10));
-		putc_vga('\n');
-	}
 
 	/*
 	 * Testing for cpu features is simple:
@@ -36,10 +45,14 @@ void kernel_main(void) {
 	 * } else {
 	 *     // feature isnt supported
 	 * }
-	puts_vga("CPU Features:\n");
-	cpu_features features = cpuFeatures();
-
 	 */
+	puts_vga("CPU Features:\n");
+
+	// We really only need to do this once. 
+	// Really this should be moved elsewhere though.
+	features = cpuFeatures();
+
+
 	// Test the panic system
 	// panic_s("This is a really long message to test the way the buffer works. qwertyuiop[]\\asdaghjkl;'zxcvbnm,./1234567890-=!@#$%^&*()_+\"?<>{}|");
 }
