399951ba1f9a191dd0783548ec290add91ca97ee
MalTheLegend104 <79492634+MalTheLegend104@users.noreply.github.com>
2023-12-19 04:26:19 -0500

    start memory stuff

--- src/kernel/kcore/kernel.cpp
+++ src/kernel/kcore/kernel.cpp
@@ -4,12 +4,10 @@
 #include <cpuid.h>
 #include <panic.h>
 #include <stdio.h>
-#include <paging.h>
 #include <klibc/kprint.h>
 #include <klibc/cpuid_calls.h>
 #include <klibc/logger.h>
 #include <klibc/features.hpp>
-#include <stdio.h>
 #include <multiboot.h>
 #include <klibc/multiboot.hpp>
 #include <idt.h>
@@ -31,49 +29,6 @@
 extern "C" {
 	void kernel_main(unsigned int magic, multiboot_info* mbt_info);
 	void __cxa_pure_virtual() { }; // needed for pure virtual functions
-}
-
-#pragma GCC diagnostic ignored "-Wunused-parameter" 
-int memtest(int argc, char** argv) {
-	multiboot_tag_mmap* mmap_tag = MultibootManager::getMMap();
-	logger(INFO, "Entry size: %d\n", mmap_tag->entry_size);
-	logger(INFO, "Entries: %d\n", mmap_tag->size / mmap_tag->entry_size);
-	struct multiboot_mmap_entry* mmap;
-	size_t total = 0, usable = 0, reserved = 0;
-	for (mmap = mmap_tag->entries; (size_t) mmap < (size_t) mmap_tag + mmap_tag->size; mmap = (struct multiboot_mmap_entry*) ((size_t) mmap + (size_t) mmap_tag->entry_size)) {
-
-		// printf("New Entry:\tBase: %X", mmap->addr);
-		// printf("\tLength: %llu", mmap->len);
-		// printf("\tType: %llu\n", mmap->type);
-		total += mmap->len;
-		if (mmap->type == MULTIBOOT_MEMORY_AVAILABLE) {
-			usable += mmap->len;
-		} else {
-			logger(WARN, "Unusable memory: Addr: %X Reason: %d Bytes: %llu\n", mmap->addr, mmap->type, mmap->len);
-			reserved += mmap->len;
-		}
-	}
-	set_colors(VGA_COLOR_PINK, VGA_DEFAULT_BG);
-	printf("Total Memory:\n");
-	set_to_last();
-	set_colors(VGA_COLOR_PURPLE, VGA_DEFAULT_BG);
-	printf("\t%llu bytes\n\t%llu KiB\n\t%llu MiB\n", total, total / 1024, (total / 1024) / 1024);
-
-	set_to_last();
-	set_colors(VGA_COLOR_LIGHT_GREEN, VGA_DEFAULT_BG);
-	printf("Usable Memory:\n");
-	set_to_last();
-	set_colors(VGA_COLOR_GREEN, VGA_DEFAULT_BG);
-	printf("\t%llu bytes\n\t%llu KiB\n\t%llu MiB\n", usable, usable / 1024, (usable / 1024) / 1024);
-
-	set_to_last();
-	set_colors(VGA_COLOR_LIGHT_RED, VGA_DEFAULT_BG);
-	printf("Reserved Memory:\n");
-	set_to_last();
-	set_colors(VGA_COLOR_RED, VGA_DEFAULT_BG);
-	printf("\t%llu bytes\n\t%llu KiB\n\t%llu MiB\n", reserved, reserved / 1000, (reserved / 1000) / 1000);
-	set_to_last();
-	return 0;
 }
 
 #pragma GCC diagnostic ignored "-Wunused-parameter" 
---
+++ src/kernel/klibc/include/memory/physical_mem.h
@@ -0,0 +1,13 @@
+#ifndef PHYSICAL_MEM_H
+#define PHYSICAL_MEM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+#endif // PHYSICAL_MEM_H
