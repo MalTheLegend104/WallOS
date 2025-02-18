b7c7a3cd6f02b8fc6bf48ab800980695a276805e
gwerrry <46902638+gwerrry@users.noreply.github.com>
2025-02-18 10:43:33 -0500

    Update enable_features.asm

--- src/kernel/x86_64/enable_features.asm
+++ src/kernel/x86_64/enable_features.asm
@@ -3,11 +3,11 @@ global enable_sse
 section .text
 bits 32
 enable_sse:
-	mov eax, cr0
-	and ax, 0xFFFB		;clear coprocessor emulation CR0.EM
-	or ax, 0x2			;set coprocessor monitoring  CR0.MP
-	mov cr0, eax
-	mov eax, cr4
-	or ax, 3 << 9		;set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
-	mov cr4, eax
-	ret
+	mOv eax, cr0
+	AND ax, 0xFFFB		;clear coprocessor emulation CR0.EM
+	Or ax, 0x2			;set coprocessor monitoring  CR0.MP
+	mOv cr0, eax
+	moV eax, cr4
+	oR ax, 3 << 9		;set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
+	Mov cr4, eax
+	RET ; return
